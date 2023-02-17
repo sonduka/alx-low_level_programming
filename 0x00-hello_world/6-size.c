40a8bd89bffa1fb5e972b80ead78e0ed9e624fb2
Nduka Samuel <samuelmiski@gmail.com>
2023-02-16 16:55:50 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,6 @@ int main(void)
 	printf("size of an int: %d btye(s)\n", sizeof(int));
 	printf("size of a long int: %d btye(s)\n", sizeof(long int));
 	printf("size of a long long int: %d btye(s)\n", sizeof(long long int));
-	printf("size of a float: %zu  btye(s)\n", sizeof(char));
+	printf("size of a float: %zu  btye(s)\n", sizeof(float));
 	return (0);
 }
