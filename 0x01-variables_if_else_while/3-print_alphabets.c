ca7f54483a472494fcafe9687488f86520a65751
Nduka Samuel <samuelmiski@gmail.com>
2023-02-17 11:05:44 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 /**
  * Main - Entry point
- * Description
+ * Description: Alphabet game 
  * Return: 0
  */
 
 int main(void)
 {
 	char i;
-	for (i = 'a'; i<= 'z'; i++)
+
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		putchar(i);
 	}
-	for (i = 'A'; i<= 'Z'; i++)
+	for (i = 'A'; i <= 'Z'; i++)
 	{
 		putchar(i);
 	}
