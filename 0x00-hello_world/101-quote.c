d850b464c59303d2e9e729b07886669a426fbdf6
Nduka Samuel <samuelmiski@gmail.com>
2023-02-16 17:34:19 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 /**
  * main - prints to string
  * Description: Prints "and that piece of art is useful" without puts
- * Return: 1
- * 
- **/
+ * Return: 1 
+ */
  
-int main(void)
-{
+ int main(void)
+ {
 	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return(1);
-}	
+ }	
