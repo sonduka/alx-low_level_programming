950a62aff44ee39d25faf29b4db6528b18341348
Nduka Samuel <samuelmiski@gmail.com>
2023-02-17 10:46:43 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description
+ * Return:0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return(0);
+}
