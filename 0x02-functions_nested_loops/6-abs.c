e6c00410e173eb8a48dc7df9e4b51a24d8cec315
Nduka Samuel <samuelmiski@gmail.com>
2023-02-21 14:36:48 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - prints signs
+ * Description - print sign
+ * @r:the character to check
+ * Return: 0
+ */
+
+int _abs(int r)
+{
+	if (r < 0)
+	{
+	return (-r);
+	}
+	else
+	{
+	return (r);
+	}
+}
