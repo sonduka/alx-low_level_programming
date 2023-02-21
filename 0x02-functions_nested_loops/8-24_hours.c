4573744d046d514f3885d82d497ebcf9bb928e2a
Nduka Samuel <samuelmiski@gmail.com>
2023-02-21 15:31:19 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - prints signs
+ * Description - print sign
+ * @n:the character to check
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+	for (minute = 0; minute < 60; minute ++)
+	{
+	 _putchar('0' + (hour / 10));
+	 _putchar('0' + (hour % 10));
+	 _putchar(':');
+	 _putchar('0' + (minute / 10));
+	 _putchar('0' + (minute % 10));
+	 _putchar('\n');
+	}
+	}
+}
