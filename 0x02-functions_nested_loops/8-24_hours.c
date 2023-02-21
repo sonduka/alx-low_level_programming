9c0840ed87d6d7cf17133b60be2bcb6455cc1d1d
Nduka Samuel <samuelmiski@gmail.com>
2023-02-21 15:49:11 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,12 +13,13 @@ void jack_bauer(void)
 	{
 		for (minute = 0; minute < 60; minute++)
 		{
-		 _putchar('0' + (hour / 10));
-		 _putchar('0' + (hour % 10));
-		 _putchar(':');
-		 _putchar('0' + (minute / 10));
-		 _putchar('0' + (minute % 10));
-		 _putchar('\n');
+			_putchar(hour / 10 + '0');
+			_putchar(hour % 10 + '0');
+			_putchar(':');
+			_putchar(minute / 10 + '0');
+			_putchar(minute % 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
+
