5e4e99be12e8247a42d1cb885bffb4fcf0297191
Nduka Samuel <samuelmiski@gmail.com>
2023-02-21 14:56:23 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_last_digit - prints signs
+ * Description - print sign
+ * @r:the character to check
+ * Return: 0
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	if (n < 0)
+	{
+	n = -n;
+	}
+
+	last_digit = n % 10;
+	_putchar (last_digit + '0');
+	return (last_digit);
+}
+
