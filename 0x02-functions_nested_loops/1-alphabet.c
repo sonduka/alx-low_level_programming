08e3f97348c26070233f2e1231bc5dab2a2d0c16
Nduka Samuel <samuelmiski@gmail.com>
2023-02-21 05:27:50 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,12 @@
  * Description - print lower case a-z
  * Return: 0
  */
+
 void print_alphabet(void)
 {
 	char i;
-	for (i = 'a'; i <= 'z'; i++ )
-		_putchar(i);
+	for (i = 'a'; i <= 'z'; i++)
+	_putchar(i);
 	
 	_putchar('\n');
 
