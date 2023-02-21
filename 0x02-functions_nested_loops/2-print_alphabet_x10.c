d2de6a8fb915ef784221c712dafb4659d8b7e806
Nduka Samuel <samuelmiski@gmail.com>
2023-02-21 11:22:55 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,10 @@
 void print_alphabet_x10(void)
 {
 	int i, j;
-	for (i = 0; i <10; i++)
+
+	for (i = 0; i < 10; i++)
 	{
-	for (j = 'a'; j <= 'z'; j++)
+	for (j = 0; j < 26; j++)
 	{
 	_putchar('a' + j);
 	}
