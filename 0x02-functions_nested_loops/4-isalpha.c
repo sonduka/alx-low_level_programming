f8a1455018f99b15fac0c75d900d38f3db2b2d65
Nduka Samuel <samuelmiski@gmail.com>
2023-02-21 11:50:11 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for upper case
+ * @c: the character to check
+ * Description - print lower case a-z
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        {
+        return (1);
+        }
+        else
+        {
+        return (0);
+        }
+}
