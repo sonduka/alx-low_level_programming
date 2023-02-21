0f5b3d0c89ac3a20651daa731c3403add8ca14ed
Nduka Samuel <samuelmiski@gmail.com>
2023-02-21 11:33:51 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower(int c) - print lower case
+ * Description - print lower case a-z
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return 1;
+	}
+	else
+	{
+	return 0;
+	}
+}
