118018a6ecb058e6e5ebbc4a37e83735b9cd2120
Nduka Samuel <samuelmiski@gmail.com>
2023-02-21 15:14:59 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,15 +5,15 @@
  * @n:the character to check
  * Return: 0
  */
+
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int lst = n % 10;
 
 	if (n < 0)
 	{
-	last_digit = last_digit *  -1;
-
-	_putchar (last_digit + '0');
-	return (last_digit);
+		lst = lst * -1;
+	}
+	_putchar(lst + '0');
+	return (lst);
 }
-
