2bb22f4106fc386ba8ecb9f58ce231a77aae0051
Nduka Samuel <samuelmiski@gmail.com>
2023-02-21 05:23:00 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,17 +6,11 @@
  */
 void print_alphabet(void)
 {
-	char letter = 'a';
-	while (letter <= 'z'){
-	_putchar('%c',letter);
-	letter++;
-	}
+	char i;
+	for (i = 'a'; i <= 'z'; i++ )
+		_putchar(i);
+	
 	_putchar("\n");
 
 }
 
-int main(void)
-{
-	print_alpabet();
-	
-}
