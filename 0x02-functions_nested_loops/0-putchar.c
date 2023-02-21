bfd4d07a61c37785ac450307fc800e4963a63c5f
Nduka Samuel <samuelmiski@gmail.com>
2023-02-21 03:21:23 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * Description- nested loops
+ * return: Always 0
+ */
+int _putchar(char c)
+{
+	return putchar(c);
+}
+
+int main()
+{
+	_putchar('_');	
+	_putchar('p');	
+	_putchar('u');	
+	_putchar('t');	
+	_putchar('c');	
+	_putchar('h');	
+	_putchar('a');	
+	_putchar('r');	
+	_putchar('\n');
+	return 0;
+}	
+
+
