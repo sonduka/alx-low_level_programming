2f1bf28459ff9e198f5abd2b11a8141aae700b63
Nduka Samuel <samuelmiski@gmail.com>
2023-02-23 23:40:10 +0100

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_remaining_days - takes a date and prints how many days are
+ * print_remaining_days - takes  date and prints how many days are
  * left in the year, taking leap years into account
  * Description: prints the date
  * @month: month in number format
