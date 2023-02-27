267a394d79da4f298afc5145d631ed863bf5540d
Nduka Samuel <samuelmiski@gmail.com>
2023-02-27 20:24:44 +0100

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,14 @@
 void print_rev(char *s)
 {
 	int len = 0;
+	int i;
 
 	while (s[len] != '\0')
 	{
 		len++;
 	}
 
-	for (int i = len -1; i >= 0; i--;)
+	for (i = len -1; i >= 0; i--;)
 	{
 		_putchar(s[i]);
 	}
