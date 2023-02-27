5a91f290f5f85a83894c2108b10e63b71e3f4b43
Nduka Samuel <samuelmiski@gmail.com>
2023-02-27 20:53:52 +0100

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * puts2 - prints every other character
+ * Description - prints
+ * @str: pointer to the string to be printed
  *
  * Return: Always 0.
  */
-int main(void)
-{
-    char *str;
 
-    str = "0123456789";
-    puts2(str);
-    return (0);
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
 }
