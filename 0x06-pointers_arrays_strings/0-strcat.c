c7e8a840d25bffba9387741da087ca733543c77e
Nduka Samuel <samuelmiski@gmail.com>
2023-03-02 00:42:10 +0100

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,15 +12,5 @@
 
 char _strcat(char* dest, char* src)
 {
-	char* p = dest;
-	while (*p != '\0')
-	{
-		p++;
-	}
-	while (*src != '\0')
-	{
-		*p++ = *src++;
-	}
-	*p = '\0';
-	return dest;
+	return (strcat(dest, src));
 }
