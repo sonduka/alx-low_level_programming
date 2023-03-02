8456812f99f8022a2f67f33b80c90973632d7aab
Nduka Samuel <samuelmiski@gmail.com>
2023-03-02 13:02:43 +0100

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,44 +1,21 @@
 #include "main.h"
 
 /**
- * *cap_string - Entry point
- * Description: Capitalizes all words of a string
+ * *string_toupper - Entry point
+ * Description: Changes all lowercase letter to uppercase
  * @str: Character
  * Return: char
  */
 
-char *cap_string(char *str)
+char *string_toupper(char *str)
 {
 	int i = 0;
-	int cap = 1; /* Capitalize the first letter */
 
 	while (str[i] != '\0')
 	{
-		switch (str[i])
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			case ' ':
-			case '\t':
-			case '\n':
-			case ',':
-			case ';':
-			case '.':
-			case '!':
-			case '?':
-			case '\"':
-			case '(':
-			case ')':
-			case '{':
-			case '}':
-				cap = 1; /* Set flag to capitalize next letter */
-				break;
-			default:
-				if (cap)
-				{
-					/* capitalize current letter */
-					str[i] = (str[i] >= 'a' && str[i] <= 'z') ? str[i] - 'a' + 'A' : str[i];
-					cap = 0; /* Clear flag*/
-				}
-				break;
+			str[i] -= 32;
 		}
 		i++;
 	}
