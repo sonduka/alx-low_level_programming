1b834974fc4b4a9bb8f3c30b93d87ddfb212eead
Nduka Samuel <samuelmiski@gmail.com>
2023-03-02 12:26:55 +0100

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies string 
+ * Description - copy string1 to string 2
+ * @src: character
+ * @dest: character
+ * Return: dest
+ */
+
+char* _strncpy(char* dest, char* src, int n)
+{
+	char* p = dest;
+	while (*src != '\0' && n > 0)
+	{
+		*p++ = *src++;
+		n--;
+	}
+
+	while (n > 0) 
+	{
+		*p++ = '\0';
+		n--;
+	}
+
+	return dest;
+}
