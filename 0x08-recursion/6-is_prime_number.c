89a19aabc3ad7924f10c907e69126e74833e2903
Nduka Samuel <samuelmiski@gmail.com>
2023-04-06 01:31:51 +0100

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,30 +1,42 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * is_divisible - Entry point
+ * Description: Prime number
+ * @n: Integer
+ * @div: Integer
+ * Return: 1 if prime number 0 otherwise
  */
-int main(void)
-{
-    int r;
 
-    r = is_prime_number(1);
-    printf("%d\n", r);
-    r = is_prime_number(1024);
-    printf("%d\n", r);
-    r = is_prime_number(16);
-    printf("%d\n", r);
-    r = is_prime_number(17);
-    printf("%d\n", r);
-    r = is_prime_number(25);
-    printf("%d\n", r);
-    r = is_prime_number(-1);
-    printf("%d\n", r);
-    r = is_prime_number(113);
-    printf("%d\n", r);
-    r = is_prime_number(7919);
-    printf("%d\n", r);
-    return (0);
+int is_divisible(int n, int div)
+{
+	if (div == 1 || div == n)
+	{
+		return (0);
+	}
+	if (n % div == 0)
+	{
+		return (1);
+	}
+	return (is_divisible(n, div - 1));
+}
+
+/**
+ * is_prime_number - Entry point
+ * Description: Prime number
+ * @n: Integer
+ * Return: 1 if prime number 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n ==  2)
+	{
+		return (1);
+	}
+	return (!is_divisible(n, n - 1));
 }
