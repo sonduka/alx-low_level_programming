8a6b263ef34f9249352beee03f7b778b422b1da9
Nduka Samuel <samuelmiski@gmail.com>
2023-04-11 02:23:12 +0100

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,19 +1,21 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
-* main - prints all arguments it recieves
-* @argc: number of arguments passed to the function
-* @argv: argument vector of pointers to strings
-*
-* Return: always 0
-*/
+ * main - Entry point
+ * Description: Prints all arguments it receives
+ * @argc: No of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
-	printf("%s\n", argv[i]);
+		printf("%s\n", argv[i]);
 	}
-
 	return (0);
 }
