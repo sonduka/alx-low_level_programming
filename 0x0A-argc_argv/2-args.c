63942bc3931967b28975cdd7dd6511d713d9bc7e
Nduka Samuel <samuelmiski@gmail.com>
2023-04-11 02:19:02 +0100

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,30 +1,18 @@
-#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - Entry point
- * Description: Multiplies two numbers
- * @argc: No of command line arguments
- * @argv: Array name
- * Return: 1 if needs more arguements, 0 otherwise
- */
-
+* main - prints all arguments it recieves
+* @argc: number of arguments passed to the function
+* @argv: argument vector of pointers to strings
+*
+* Return: always 0
+*/
 int main(int argc, char *argv[])
 {
-	int n1, n2, ans;
-
-	if (argc != 3)
+	int i;
+	for (i = 0; i < argc; i++)
 	{
-		printf("Error\n");
-		return (1);
+	printf("%s\n", argv[i]);
 	}
-
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[2]);
-	ans = n1 * n2;
-
-	printf("%d\n", ans);
-
 	return (0);
 }
